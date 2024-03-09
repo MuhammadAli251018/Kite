f57d39a267654b1b88eb85d1349fef72190a0844
MuhammadAli251018 <muhammadali251018@gmail.com>
2024-03-10 00:02:04 +0200

    Add MonogoDB & Serialization dependency

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlinSerialization)
     application
 }
 
@@ -16,6 +17,14 @@ dependencies {
     implementation(libs.logback)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
+
+    //  Monogo DB
+    implementation(libs.mongodb.driver.kotlin.coroutine)
+    implementation(libs.bson.kotlinx)
+
+    //  Kotlin Serialization
+    implementation(libs.kotlinx.serialization.json)
+
     testImplementation(libs.ktor.server.tests)
     testImplementation(libs.kotlin.test.junit)
 }
