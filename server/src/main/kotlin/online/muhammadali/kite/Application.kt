b40d0d6b7de568d25c8ac6f011599f8703b00191
MuhammadAli251018 <muhammadali251018@gmail.com>
2024-03-18 22:49:29 +0200

    Update Server to work with EngineMain

--- server/src/main/kotlin/online/muhammadali/kite/Application.kt
+++ server/src/main/kotlin/online/muhammadali/kite/Application.kt
@@ -1,22 +1,16 @@
 package online.muhammadali.kite
 
-import SERVER_PORT
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import online.muhammadali.kite.plugins.configureAuthentication
 import online.muhammadali.kite.plugins.configureDi
 import online.muhammadali.kite.plugins.configureRouting
 import online.muhammadali.kite.plugins.configureSerialization
 
-fun main( args: Array<String>): Unit = EngineMain.main(args)/* {
-    embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-
-}*/
+fun main( args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
-    println(environment.config.propertyOrNull("jwt.domain"))
+    println(environment.config.propertyOrNull("jwt.domain")?.getString())
     configureDi()
     configureAuthentication()
     configureSerialization()
